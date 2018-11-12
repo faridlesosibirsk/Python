75982ff4688eaa60d8e1ba20a5803656b9b6e28e
faridlesosibirsk <farid.lfsibgtu.ru@mail.ru>
2018-11-12 21:56:27 +0700

    изменено:      1.py

--- 1.py
+++ 1.py
@@ -1,9 +1,15 @@
+import re
+
 def reader(filename):
+
+    regexp = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
 
     with open(filename) as f:
         log = f.read()
 
-    print(log)
+        ips_list = re.findall(regexp,log)
+
+    print(ips_list)
 
 if __name__ == '__main__':
     reader('access.log.1')
